4b89abbc064da25b953297e7bf7d61f09d06968a
kiranpranay <kiranpranay12@gmail.com>
2024-11-21 19:43:35 +0530

    hello

--- Script.sh
+++ Script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-sudo apt install php-cli
+# sudo apt install php-cli
 # Define host and port
-HOST="0.0.0.0"
-PORT="8080"
+# HOST="0.0.0.0"
+# PORT="8080"
 
 
 # Start the PHP built-in server
 echo "Starting PHP server on $HOST:$PORT"
 
-php -S "$HOST:$PORT"
+# php -S "$HOST:$PORT"
