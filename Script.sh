84f9a19274183f6224805e69d909944361e66fdb
kiranpranay <kiranpranay12@gmail.com>
2024-11-21 19:53:54 +0530

    sudo from begining

--- Script.sh
+++ Script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Switch to sudo user from the beginning
+sudo -v || { echo "Failed to switch to sudo user"; exit 1; }
+
 # Print starting message
 echo "I am starting the game now"
 
